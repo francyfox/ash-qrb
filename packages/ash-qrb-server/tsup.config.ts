11209d93f52c601d4f3a55e484fffffb4286deef
Danil Golota <info@shalotts.site>
2024-06-25 14:18:51 +0500

    feat: test edge plugins

--- packages/ash-qrb-server/tsup.config.ts
+++ packages/ash-qrb-server/tsup.config.ts
@@ -4,6 +4,9 @@ export default defineConfig({
   entry: ['./src/index.ts'],
   outDir: './dist',
   format: ['esm'],
+  target: 'node16',
+  platform: 'node',
+  noExternal: [/(.*)/],
   minify: true,
   clean: true,
 })
