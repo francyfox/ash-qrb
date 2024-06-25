7e588d78ccf6b94c01156aaf13a9d6d03edc1bdc
Danil Golota <info@shalotts.site>
2024-06-25 14:31:15 +0500

    feat: test edge plugins

--- packages/ash-qrb-server/src/env.ts
+++ packages/ash-qrb-server/src/env.ts
@@ -14,6 +14,13 @@ const envVariables = z.object({
   XATA_DB_PG_HOST: z.string(),
 })
 
-console.log(process.env)
+// export const env = envVariables.parse(process.env)
 
-export const env = envVariables.parse(process.env)
+export const env = {
+  RUNTIME: 'edge',
+  NODE_ENV: 'production',
+  XATA_DB_HTTP_HOST:
+    'https://Danil-Golota-s-workspace-oddvjb.eu-west-1.xata.sh/db/ashqrb:main',
+  XATA_DB_PG_HOST:
+    'postgresql://oddvjb:xau_tubjnzw7epVbvzBGS8qlmVOi0mhaLVuP2@eu-west-1.sql.xata.sh/ashqrb:main?sslmode=require',
+}
