9a19c078241cdee0ea1a2f0c2bf2b7a89f3a5036
Turbobot <turbobot@vercel.com>
2024-11-29 18:06:35 +0500

    feat(create-turbo): apply official-starter transform

--- packages/ash-qrb-server/src/module/db/db.ts
+++
@@ -1,7 +0,0 @@
-import { env } from '@root/env'
-import { drizzle } from 'drizzle-orm/postgres-js'
-import postgres from 'postgres'
-
-// for query purposes
-const client = postgres(env.XATA_DB_PG_HOST, { max: 1 })
-export const db = drizzle(client)
