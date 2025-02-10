d02b78f6aa8faac6d7a77450d55a87c0edddba91
Turbobot <turbobot@vercel.com>
2025-02-10 19:51:15 +0500

    feat(create-turbo): apply official-starter transform

--- packages/ash-qrb-client/src/module/repository/repository.client.ts
+++
@@ -1,5 +0,0 @@
-import { ENV } from '@root/env'
-import type { components, paths } from 'openapi-dts/openapi/api'
-import createClient from 'openapi-fetch'
-const ApiClient = createClient<paths>({ baseUrl: `${ENV.NEXT_API_URL}` })
-export default ApiClient
--- packages/ash-qrb-server/drizzle.config.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'drizzle-kit'
-export default defineConfig({
-  dialect: 'postgresql',
-  schema: './src/module/**/*.schema.ts',
-  out: './drizzle',
-  dbCredentials: {
-    url: process.env.XATA_DB_PG_HOST ?? '',
-  },
-})
