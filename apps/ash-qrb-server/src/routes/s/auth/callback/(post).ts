fa1dff2f8ba3d2b8c83c48bb94c06071a9044fe2
danilgolota <7info7web@gmail.com>
2025-02-13 17:08:38 +0500

    init nuxt

--- apps/ash-qrb-server/src/routes/s/auth/callback/(post).ts
+++ apps/ash-qrb-server/src/routes/s/auth/callback/(post).ts
@@ -2,20 +2,17 @@ import { config } from '@/config.ts'
 import type { ElysiaApp } from '@/server.ts'
 
 export default (app: ElysiaApp) =>
-  app.post(
-    '/',
-    async ({ redirect, oauth2, cookie: { redirectUrl, userRefreshToken } }) => {
-      const tokens = await oauth2.authorize('WorkOS')
+  app.post('/', async ({ redirect, oauth2, cookie: { userRefreshToken } }) => {
+    const tokens = await oauth2.authorize('WorkOS')
 
-      if (tokens.hasRefreshToken() && userRefreshToken) {
-        userRefreshToken.set({
-          value: tokens.refreshToken(),
-          secure: true,
-          httpOnly: true,
-          sameSite: 'strict',
-        })
-      }
+    if (tokens.hasRefreshToken() && userRefreshToken) {
+      userRefreshToken.set({
+        value: tokens.refreshToken(),
+        secure: true,
+        httpOnly: true,
+        sameSite: 'strict',
+      })
+    }
 
-      return redirect(config.CLIENT_APP_URL)
-    },
-  )
+    return redirect(config.CLIENT_APP_URL)
+  })
