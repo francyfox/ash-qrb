80f74716ae5bd6a67f4838cdb18e65f4bddc6df4
danilgolota <7info7web@gmail.com>
2025-02-24 18:32:19 +0500

    (feat) default card

--- apps/ash-qrb-client/app/components/cards/DefaultCard.vue
+++ apps/ash-qrb-client/app/components/cards/DefaultCard.vue
@@ -2,18 +2,33 @@
   const props = defineProps<{
     title: string
   }>()
+
+  const slots = useSlots()
+
+  const hasCardFooterSlot = !!slots['footer']
+  const hasCardContentSlot = !!slots['content']
 </script>
 
 <template>
-  <div class="default-card flex flex-col items-center gap-2.5 self-stretch px-10 py-5">
-    <div v-if="title" class="default-card-title text-[color:var(--app-s-purple-taupe,#49454A)] font-sofia text-4xl font-normal leading-[54px]">
-      {{ title }}
-    </div>
+  <div class="default-card flex w-[560px] max-w-screen-sm flex-col items-start gap-2.5 rounded-[var(--Corner-Small,8px)] [background:linear-gradient(180deg,var(--app-s-dark-vanilla,#DACBB1)_0%,var(--app-s-champagne,#F1EACE)_100%)] shadow-[0px_25px_50px_0px_rgba(0,0,0,0.25)]">
+    <div class="default-card-container flex flex-col items-center self-stretch">
+      <div v-if="title" class="default-card-title text-[color:var(--app-s-purple-taupe,#49454A)] font-sofia text-4xl font-normal leading-[54px]">
+        {{ title }}
+      </div>
 
-    <slot />
+      <div
+          v-if="hasCardContentSlot"
+          class="default-card-content"
+      >
+        <slot />
+      </div>
 
-    <div class="default-card-footer flex flex-col justify-center items-center self-stretch [background:linear-gradient(180deg,#49454A_0%,#AEA4B0_349.4%)] px-10 py-2.5">
-      <slot name="footer" />
+      <div
+          v-if="hasCardFooterSlot"
+          class="default-card-footer flex flex-col justify-center items-center self-stretch [background:linear-gradient(180deg,#49454A_0%,#AEA4B0_349.4%)] px-10 py-2.5"
+      >
+        <slot name="footer" />
+      </div>
     </div>
   </div>
 </template>
--- apps/ash-qrb-client/app/pages/index.vue
+++ apps/ash-qrb-client/app/pages/index.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
+import DefaultCard from '~/components/cards/DefaultCard.vue'
 </script>
 
 <template>
   <section class="section-auth">
     <div class="container">
+      <DefaultCard title="test">
+        test
+      </DefaultCard>
       <h1 class="text-4xl text-white">test</h1>
       <h2 class="font-sofia text-2xl text-pFawn">sssss</h2>
     </div>
