ea17696b15c3f347b22a8b5ecf6f92f59eb40e90
danilgolota <7info7web@gmail.com>
2025-05-02 16:35:21 +0500

    feat: admin layout

--- apps/ash-qrb-client/app/layouts/AdminPanel.vue
+++ apps/ash-qrb-client/app/layouts/AdminPanel.vue
@@ -1,12 +1,53 @@
 <script setup lang="ts">
+import PanelCard from '~/components/ui/cards/PanelCard.vue'
 </script>
 
 <template>
-  <div class="admin-layout">
-    <slot />
-  </div>
+  <section class="admin-layout h-full">
+    <div class="container flex items-center h-full">
+      <div class="flex w-full min-h-[80vh] gap-5">
+        <PanelCard class="card-content overflow-hidden">
+          <slot />
+        </PanelCard>
+
+        <PanelCard class="card-menu max-w-[335px] overflow-hidden">
+
+        </PanelCard>
+      </div>
+    </div>
+  </section>
 </template>
 
-<style scoped>
+<style lang="postcss" scoped>
+.card-content {
+  position: relative;
 
+  &:after {
+    top: 0;
+    left: 0;
+    content: '';
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    opacity: .2;
+    background: url("@/assets/images/bg-content.jpg") center / cover no-repeat;
+  }
+}
+
+.card-menu {
+  position: relative;
+
+  &:after {
+    top: 0;
+    left: 0;
+    content: '';
+    position: absolute;
+    display: block;
+    width: 100%;
+    height: 100%;
+    opacity: .2;
+    background-image: url("@/assets/images/bg-menu.jpg");
+  }
+}
 </style>
--- apps/ash-qrb-client/app/pages/(security)/dashboard/index.vue
+++ apps/ash-qrb-client/app/pages/(security)/dashboard/index.vue
@@ -9,13 +9,9 @@ const session = authClient.useSession()
 </script>
 
 <template>
-  <div class="text-4xl text-white text-center">
-    WELCOME {{ session.data?.user?.email }}
-    
-    <button v-if="session.data" @click="authClient.signOut()">
-      Sign out
-    </button>
-  </div>
+  <NuxtLayout name="admin-panel">
+
+  </NuxtLayout>
 </template>
 
 <style scoped>
