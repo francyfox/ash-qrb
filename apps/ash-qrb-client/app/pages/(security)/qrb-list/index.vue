fb130864c7012d3284a71e4a0597e9fe0c134523
francyfox <7info7web@gmail.com>
2025-05-19 15:11:23 +0500

    feat: qrb list

--- apps/ash-qrb-client/app/pages/(security)/qrb-list/index.vue
+++ apps/ash-qrb-client/app/pages/(security)/qrb-list/index.vue
@@ -1,8 +1,25 @@
 <script setup lang="ts">
+import QrbList from '~/components/ui/table/QrbList.vue'
+
+const { t } = useI18n()
 </script>
 
 <template>
   <NuxtLayout name="admin-panel">
+    <div class="relative z-10 h-full flex flex-col justify-between gap-5">
+      <div class="flex">
+        <UButton
+            color="primary"
+            type="button"
+            class="cursor-pointer"
+            icon="i-lucide-qr-code"
+        >
+          {{ t('qrbListAdd') }}
+        </UButton>
+      </div>
+
+      <QrbList />
+    </div>
   </NuxtLayout>
 </template>
 
