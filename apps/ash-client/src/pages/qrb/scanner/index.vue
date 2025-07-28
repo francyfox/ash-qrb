83b69c0c1c21b714e044c0c9fea0d495c27f81b4
francyfox <7info7web@gmail.com>
2025-07-28 18:39:47 +0500

    feat: add scanner page

--- apps/ash-client/src/pages/qrb/scanner/index.vue
+++ apps/ash-client/src/pages/qrb/scanner/index.vue
@@ -1,11 +1,19 @@
 <script setup lang="ts">
+const toast = useToast()
 
+const providers = {
+  toast,
+}
 </script>
 
 <template>
-  <section class="section-scanner">
+  <section class="section-scanner mt-5">
     <div class="container">
-
+      <DefaultCard class="py-5">
+        <Scanner
+            v-bind="{ providers }"
+        />
+      </DefaultCard>
     </div>
   </section>
 </template>
